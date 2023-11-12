319f498682cd0b24a961e8e4be2c46f049f1509d
Karimatif33 <karimatif33@gmail.com>
2023-11-12 07:25:17 +0200

    adding a key

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ function handleAddFriend(friend) {
           {showAddFriend ? "Close" : "Add Friend"}
         </Button>
       </div>
-{ selectedFriend && <FormSplitBill selectedFriend={selectedFriend} onSplitBill={handleSplitBill} />
+{ selectedFriend && <FormSplitBill selectedFriend={selectedFriend} onSplitBill={handleSplitBill} key={selectedFriend.id} />
 }    </div>
   );
 }
